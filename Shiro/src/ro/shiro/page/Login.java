83598868e637da23e02ed94a3fb6213b25ce59aa
edi.gitman@gmail.com <edi.gitman@gmail.com@d365de16-043b-9871-17d1-5bf8edbddaa8>
2012-07-18 14:14:07 +0000

    Rename index to login

--- Shiro/src/ro/shiro/page/Login.java
+++ Shiro/src/ro/shiro/page/Login.java
@@ -2,6 +2,7 @@ package ro.shiro.page;
 
 import org.apache.click.control.FieldSet;
 import org.apache.click.control.Form;
+import org.apache.click.control.Label;
 import org.apache.click.control.PasswordField;
 import org.apache.click.control.Submit;
 import org.apache.click.control.TextField;
@@ -10,18 +11,18 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
-public class Index extends org.apache.click.Page {
+import ro.shiro.page.p.Main;
+
+public class Login extends org.apache.click.Page {
 
 	private static final long serialVersionUID = 1285577893064407025L;
 	private Form f = new Form("form");
 
-	public Index() {
+	public Login() {
 		FieldSet fs = new FieldSet("Login");
-
 		fs.add(new TextField("name", true));
 		fs.add(new PasswordField("pass"));
 		fs.add(new Submit("ok", this, "onOK"));
-
 		f.add(fs);
 		addControl(f);
 	}
@@ -29,15 +30,14 @@ public class Index extends org.apache.click.Page {
 	public boolean onOK() {
 		AuthenticationToken token = new UsernamePasswordToken(
 				f.getFieldValue("name"), f.getFieldValue("pass"));
-		// 2. Get the current Subject:
 		Subject currentUser = SecurityUtils.getSubject();
-
-		// 3. Login:
 		try {
 			currentUser.login(token);
+			setRedirect(Main.class);
+			return false;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 
 		return true;
 	}
